bde69fd1daf36056a9ff001bddd2bbbf65daf724
GFkay <karim.boudjellab12@gmail.com>
2025-02-18 23:16:10 +0100

    Ajout des fonctionnalités de wishlist et review

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
